ce175348305098f85b497fa1881dd29144e88f0e
Jyri Konttinen <jyri.konttinen@gmail.com>
2023-04-04 10:31:23 +0300

    fix aria-hidden on iconGroup

--- src/IconGroup.js
+++ src/IconGroup.js
@@ -1,7 +1,7 @@
 const IconGroup = (props) => {
     const icons = [];
     for (let i = 0; i < props.amount; i++) {
-        icons.push(<i className={`material-icons ${props.type}`}>{props.name}</i>);
+        icons.push(<i className={`material-icons ${props.type}`} aria-hidden="true" >{props.name}</i>);
     }
     return icons;
 };
