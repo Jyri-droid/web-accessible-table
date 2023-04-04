c59f3ed4037d7762f583089f207fa6a50ce7ad81
Jyri Konttinen <jyri.konttinen@gmail.com>
2023-04-04 11:18:44 +0300

    fix aria-label

--- src/Table.js
+++ src/Table.js
@@ -8,7 +8,7 @@ const Table = () => {
         <caption></caption>
         <thead>
           <tr>
-            <th scope="col"></th>
+            <th scope="col">Name</th>
             <th scope="col">Risk</th>
             <th scope="col">Rating</th>
             <th scope="col">Sustainability</th>
@@ -28,7 +28,8 @@ const Table = () => {
             <th scope="row" className="rowHeader">
               Fund 1
             </th>
-            <td aria-label="Level 5">
+            <td>
+              <span aria-label="Level 6" />
               <IconGroup amount={6} type="risk" name="noise_control_off"/>
             </td>
             <td>
